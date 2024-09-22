cb424832b858756e0c9b068d2f0fe6f9263b8f0c
araag2 <artur.guimas@gmail.com>
2024-09-23 00:29:05 +0100

    small fix

--- src/inference/output_labels.py
+++ src/inference/output_labels.py
@@ -12,23 +12,17 @@ from ..evaluation.SemEval2024_sourceEval import calc_scores
 from datetime import datetime
 from tqdm import tqdm
 
-def tokenize_generate_decode_no_sample(model : object, tokenizer : object, text : str, max_new_tokens : int = 5) -> str:   
-    tokenized = tokenizer(text, return_tensors="pt")
-    tokenized["input_ids"] = tokenized.input_ids.to(device="cuda")
-    tokenized["attention_mask"] = tokenized.attention_mask.to(device="cuda")
-
-    # We could use do_sample=False and disable top_k and top_p to get a deterministic output
-    outputs = model.generate(**tokenized, max_new_tokens=max_new_tokens, do_sample=False, pad_token_id=tokenizer.eos_token_id)
-    
-    return tokenizer.decode(outputs[0][tokenized["input_ids"].shape[1]:]).strip()
-
 def tokenize_generate_decode(model : object, tokenizer : object, text : str, max_new_tokens : int = 50, top_k : int = 50, top_p : float = 0.95, do_sample : bool = True, temperature : float = 1.0) -> str:   
     tokenized = tokenizer(text, return_tensors="pt")
     tokenized["input_ids"] = tokenized.input_ids.to(device="cuda")
     tokenized["attention_mask"] = tokenized.attention_mask.to(device="cuda")
 
-    # We could use do_sample=False and disable top_k and top_p to get a deterministic output
-    outputs = model.generate(**tokenized, max_new_tokens=max_new_tokens, top_k = top_k, top_p = top_p, do_sample=do_sample, temperature = temperature, pad_token_id=tokenizer.eos_token_id)
+    outputs = None
+    if do_sample:
+        # We could use do_sample=False and disable top_k and top_p to get a deterministic output
+        outputs = model.generate(**tokenized, max_new_tokens=max_new_tokens, top_k = top_k, top_p = top_p, do_sample=do_sample, temperature = temperature, pad_token_id=tokenizer.eos_token_id)
+    else:
+        outputs = model.generate(**tokenized, max_new_tokens=max_new_tokens, do_sample=False, pad_token_id=tokenizer.eos_token_id)
     
     return tokenizer.decode(outputs[0][tokenized["input_ids"].shape[1]:]).strip()
 
@@ -42,17 +36,12 @@ def tokenize_generate_five_decode(model : object, tokenizer : object, text : str
 
     return [re.sub("(</s>)*", "", tokenizer.decode(out[tokenized["input_ids"].shape[1]:]).strip()) for out in outputs]
 
-def query_inference(model : object, tokenizer : object, queries : dict, sample : bool = True) -> dict:
+def query_inference(model : object, tokenizer : object, queries : dict, do_sample : bool = True) -> dict:
     res_labels = {}
 
     with torch.inference_mode():
         for q_id in tqdm(queries):
-            decoded_output = ""
-
-            if sample:
-                decoded_output = tokenize_generate_decode(model, tokenizer, queries[q_id]["text"], 5, 15, 0.7, True)
-            else:
-                decoded_output = tokenize_generate_decode_no_sample(model, tokenizer, queries[q_id]["text"], 5)
+            decoded_output = tokenize_generate_decode(model, tokenizer, queries[q_id]["text"], 5, 15, 0.7, do_sample)
 
             decoded_output_sub = re.sub("(<\/s>)+", " ", decoded_output)
 
